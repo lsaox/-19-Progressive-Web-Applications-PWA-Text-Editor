0159e585bd5a94f5b23a5483e9a62c5b9a392501
unknown <el_word9@yahoo.com>
2024-03-16 13:19:31 -0500

    finished adding some logic

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,9 +15,13 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
 console.log('PUT in database');
-const jateDb=  await openDB('jate', 1);
-const tx=await jateDb.transaction(['jate'],'readwrite');
-const objStore=tx.objectStore('jate');
+const jateDb =  await openDB('jate', 1);
+const tx = await jateDb.transaction(['jate'],'write');
+const store = tx.objectStore('jate');
+const request = store.put({id: 1, value: content });
+const result = await request;
+console.log('Data has been saved'    + result.value.value);
+return result;f
 }
 
 // TODO: Add logic for a method that gets all the content from the database
