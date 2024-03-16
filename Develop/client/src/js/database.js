988ec4ff16d17463b804462fdf2dffc2e3a47173
unknown <el_word9@yahoo.com>
2024-03-16 13:24:19 -0500

    fixed more mistakes and added method that gets all content from database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -20,11 +20,19 @@ const tx = await jateDb.transaction(['jate'],'write');
 const store = tx.objectStore('jate');
 const request = store.put({id: 1, value: content });
 const result = await request;
-console.log('Data has been saved'    + result.value.value);
+console.log('Data has been saved'    + result.value);
 return result;
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+console.log('GET from database')
+const jateDb = await openDB('jate', 1);
+const tx = jateDb.transaction ('jate',"readonly");
+const store  = tx.objectStore("jate");
+const request = store.get(1);
+return request;
+};
+
 
 initdb();
